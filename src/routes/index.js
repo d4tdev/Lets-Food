11a71a62e24f8f524570b665eef7d6f449314a9f
d4rtj <jsmsj04@gmail.com>
2022-10-07 00:19:02 +0700

    Đạt - completed login

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,16 +2,13 @@ const authRouter = require('./auth');
 const { validAuth } = require('../middleware/checkAuthentication');
 
 const routes = app => {
-	app.use('/auth', validAuth, authRouter);
-	app.use(
-		'/home',
-		(req, res) => {
-			res.json(req.user);
-		}
-	);
-   app.use('/', (req, res) => {
-      res.render('home', {user: req.user});
-   });
+	app.use('/auth', authRouter);
+	app.use('/home', validAuth, (req, res) => {
+		res.json(req.user);
+	});
+	app.use('/', (req, res) => {
+		res.render('home', { user: req.user });
+	});
 };
 
 module.exports = routes;
