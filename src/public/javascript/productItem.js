a44576748e94b1e21b000749dfeca79bf028f45a
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2022-10-27 22:29:01 +0700

    Hồng Đức - Update One Product

--- src/public/javascript/productItem.js
+++ src/public/javascript/productItem.js
@@ -1,11 +1,7 @@
-const productImage = document.querySelector('.product__img');
-const productNameTitle = document.querySelector('.product-name');
-const productName = document.querySelector('.product__name');
-const productDesc = document.querySelector('.product__desc');
-const productPrice = document.querySelector('.product__price--old');
+const addCartBtn = document.querySelector('.btn__add-cart');
 const sliderProduct = document.querySelector('.slider__product__list');
-const productDescIngredient = document.querySelector('.product__desc-ingredient');
-const productDescProcess = document.querySelector('.product__desc-process');
+
+let login = false;
 
 const formatPrice = price => {
    const formatter = new Intl.NumberFormat('vi-VN', {
@@ -15,31 +11,37 @@ const formatPrice = price => {
    return formatter.format(price);
 };
 
+const checkLogin = () => {
+   fetch('https://letsfood.click/check_login')
+      .then(res => res.json())
+      .then(data => {
+         if (data) {
+            return (login = true);
+         } else {
+            return (login = false);
+         }
+      });
+};
+
+checkLogin();
+
+addCartBtn.addEventListener('click', () => {
+   if (!login) {
+      addCartBtn.href = '/auth/getLoginLocal';
+   } else {
+      addCartBtn.href = `/cart/create/${addCartBtn.dataset.id}`;
+   }
+});
 
 fetch('https://letsfood.click/product/get')
    .then(response => response.json())
    .then(data => {
       console.log(data);
-      // lấy sản phẩm đầu tiên
-      let product = data[0];
-
-      productNameTitle.innerHTML = product.name;
-
-      productImage.src = product.image;
-      productImage.alt = product.name;
-      productName.textContent = product.name;
-      productDesc.textContent = product.description;
-      productPrice.textContent = formatPrice(product.price);
-      productDescIngredient.textContent = product.ingredient;
-      productDescProcess.textContent = product.process;
-
-      // hiện thị tên sản phẩm thành title của trang
-      document.title = product.name;
 
       // hiện thị sản phẩm còn lại
       data.forEach(item => {
          sliderProduct.innerHTML += `
-            <a class="slider__product__item">
+            <a class="slider__product__item" href="/product/get_one_product/${item._id}">
                   <img src="${item.image}" alt="${item.name}" class="slider__product__item-img">
                   <div class="slider__product__item-content">
                      <h4 class="slider__product__item-name">
