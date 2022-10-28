ccf07abc6a29656ce5b70082ea7db1f7c3b6ebd3
d4rtj <jsmsj04@gmail.com>
2022-10-28 08:57:43 +0700

    DDatj - check fix bug

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -58,7 +58,7 @@ class CartController {
          const cart = await cartService.updateQuantityPlus(productId, _id);
 
          // return res.status(200).json(cart);
-         res.redirect('/cart/show/' + _id);
+         // res.redirect('/cart/show/' + _id);
          return res.render('gioHang', { cart, user: req.user, message: '' });
       } catch (e) {
          return res.render('gioHang', { message: e.message });
@@ -76,7 +76,7 @@ class CartController {
          const cart = await cartService.updateQuantityMinus(productId, _id);
 
          // return res.status(200).json(cart);
-         res.redirect('/cart/show/' + _id);
+         // res.redirect('/cart/show/' + _id);
          return res.render('gioHang', { cart, user: req.user, message: '' });
       } catch (e) {
          return res.render('gioHang', { message: e.message });
